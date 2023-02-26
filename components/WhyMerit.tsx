5ed344fbc329205f0a3f7ec06962f105011fd3a4
abdul kabugu <kabuguabdul2@gmail.com>
2023-02-26 17:17:28 +0300

    Fix : removed  unsupported character

--- components/WhyMerit.tsx
+++ components/WhyMerit.tsx
@@ -8,7 +8,7 @@ export default function WhyMerit() {
         <h1 className='font-bold text-4xl mb-7'>Why Use  Merit</h1>
 
           <p className='font-semibold text-xl text-black/75'>
-          Merit badges are the new gold stars! Recognize and incentivize meaningful actions with decentralized badges that are easy to create, verify, and share. Whether you're tracking employee achievements, recognizing volunteer contributions, or incentivizing customer engagement, Merit makes it simple and rewarding. Transform how you recognize and reward achievements with Merit, and build trust and loyalty along the way. Ready to start badge-ifying your world? Try Merit today!
+          Merit badges are the new gold stars! Recognize and incentivize meaningful actions with decentralized badges that are easy to create, verify, and share. Whether youre tracking employee achievements, recognizing volunteer contributions, or incentivizing customer engagement, Merit makes it simple and rewarding. Transform how you recognize and reward achievements with Merit, and build trust and loyalty along the way. Ready to start badge-ifying your world? Try Merit today!
           </p>
           </div>
     </motion.div>
